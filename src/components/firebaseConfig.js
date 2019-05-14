787dce3bd16cf831929a440d428d21ab38d32efa
Student <example@example.com>
2019-05-14 09:01:23 -0700

    add firebase config

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBA75wCa6vOwKDUvsKdlP8YuCct1WU3phE",
+  authDomain: "flappy-bird-92c21.firebaseapp.com",
+  databaseURL: "https://flappy-bird-92c21.firebaseio.com",
+  projectId: "flappy-bird-92c21",
+  storageBucket: "flappy-bird-92c21.appspot.com",
+  messagingSenderId: "604735449759",
+  appId: "1:604735449759:web:27fde7dae85dfad5"
+};
+
+export default firebaseConfig;
