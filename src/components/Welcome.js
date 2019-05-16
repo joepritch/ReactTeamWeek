8327403168b9860aa96d0590c37e8a72d41d3988
kohichi horii <kohichi.horii@gmail.com>
2019-05-16 10:29:49 -0700

    Add description and also projects goal to readme

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,7 +1,38 @@
 import React from 'react';
-import { StyleSheet, Text, View, ImageBackground, Button, Alert } from 'react-native';
-import background from '../../assets/birds.gif'
+import { StyleSheet, Text, View, ImageBackground, Button, Animated } from 'react-native';
+import background from '../../assets/background.jpg'
 import { createAppContainer, createStackNavigator, StackActions, NavigationActions } from 'react-navigation';
+
+class FadeInView extends React.Component {
+  state = {
+    fadeAnim: new Animated.Value(0),  // Initial value for opacity: 0
+  }
+
+  componentDidMount() {
+    Animated.timing(                  // Animate over time
+      this.state.fadeAnim,            // The animated value to drive
+      {
+        toValue: 1,                   // Animate to opacity: 1 (opaque)
+        duration: 1500,              // Make it take a while
+      }
+    ).start();                        // Starts the animation
+  }
+
+  render() {
+    let { fadeAnim } = this.state;
+
+    return (
+      <Animated.View                 // Special animatable View
+        style={{
+          ...this.props.style,
+          opacity: fadeAnim,         // Bind opacity to animated value
+        }}
+      >
+        {this.props.children}
+      </Animated.View>
+    );
+  }
+}
 
 export default class Welcome extends React.Component {
       static navigationOptions = {
@@ -10,46 +41,49 @@ export default class Welcome extends React.Component {
             color: 'white'
           },
           headerStyle: {
-            backgroundColor: 'rgba(135,206,250, 0.7)',
+            backgroundColor: '#4EC0CB',
           }
         }
       render() {
 
         return (
-          <View style={styles.container}>
+          <View>
             <View style={styles.background}>
               <ImageBackground source={background} style={{width: '100%', height: '100%'}}>
               </ImageBackground>
             < /View>
               <View style={styles.buttonContainer}>
-              <Button color="#FFFFFF"
-                title="START"
-                onPress={() => {
-                  this.props.navigation.dispatch(StackActions.reset({
-                    index: 0,
-                    actions: [
-                      NavigationActions.navigate({ routeName: 'Menu' })
-                    ],
-                  }))
-                }}
-              />
-            </View>
+              <FadeInView style={styles.button}>
+                <Button color="#FFFFFF"
+                  title="START"
+                  onPress={() => {
+                    this.props.navigation.dispatch(StackActions.reset({
+                      index: 0,
+                      actions: [
+                        NavigationActions.navigate({ routeName: 'Menu' })
+                      ],
+                    }))
+                  }}
+                />
+              </FadeInView>
+              </View>
           </ View>
         );
       }
     }
 
     const styles = StyleSheet.create({
-      container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignContent: 'center',
-      },
       buttonContainer: {
         position: 'absolute',
-        justifyContent: 'center',
+        flexDirection: 'column',
         alignSelf: 'center',
-        backgroundColor: 'rgba(135,206,250, 0.5)',
+        justifyContent: 'center',
+        height: '100%',
+        width: '40%'
+      },
+      button: {
+        backgroundColor: 'rgba(197, 239, 247, 0.8)',
+        marginBottom: 10,
         borderRadius: 10,
         padding: 10,
         shadowColor: '#000000',
@@ -59,8 +93,5 @@ export default class Welcome extends React.Component {
         },
         shadowRadius: 10,
         shadowOpacity: 0.25
-      },
-      background: {
-        position: 'relative'
       }
     })
