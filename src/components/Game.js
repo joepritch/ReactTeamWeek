feab626653f2e3944a4cc9e873171f67eb8d00e7
Joe Pritchett <joempritchett@gmail.com>
2019-05-15 20:46:08 -0700

    lift pipe state

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Animated, Easing } from 'react-native';
+import { StyleSheet, Text, View, Animated, Easing, Dimensions } from 'react-native';
 import Ball from './Ball';
 import Controls from './Controls';
 import Pipe from './Pipe';
@@ -10,8 +10,35 @@ export default class Game extends React.Component{
     super(props);
     this.state = {
       BallPosition: new Animated.Value(200),
+      PipePosition: new Animated.Value(400),
+      bottomValue: '15%',
     }
   this.handleScreenTapped = this.handleScreenTapped.bind(this);
+  this.pipeItUp = this.pipeItUp.bind(this)
+  this.update = this.update.bind(this)
+  }
+
+  componentDidMount() {
+    this.update();
+    setInterval(this.pipeItUp, 2000)
+  }
+
+  update(){
+    width = Dimensions.get('window').width
+    this.setState({PipePosition: new Animated.Value(width)})
+  }
+
+  pipeItUp() {
+    let newVal = Math.floor(Math.random()*46 + 10);
+    let newValToString = newVal.toString() + '%'
+    this.setState({bottomValue: newValToString});
+    Animated.timing(
+      this.state.PipePosition,
+      {
+        toValue: -75,
+        duration: 1750
+      }).start();
+    setTimeout(this.update, 1750)
   }
 
   handleScreenTapped(){
@@ -40,7 +67,7 @@ export default class Game extends React.Component{
       <View style={styles.container}>
         <Ball BallPosition={this.state.BallPosition}/>
         <Controls onScreenTapped={this.handleScreenTapped}/>
-        <Pipe />
+        <Pipe pipeItUp={this.pipeItUp} PipePosition={this.state.PipePosition} bottomValue={this.state.bottomValue} />
       </View>
     )
   }
--- src/components/Pipe.js
+++ src/components/Pipe.js
@@ -1,50 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View, Animated, Easing } from 'react-native';
+import { StyleSheet, Text, View, Animated, Easing, Dimensions } from 'react-native';
 
 export default class Pipe extends React.Component {
-  
-  constructor(props) {
-    super(props);
-    this.state = {
-      pipePosition: new Animated.Value(400),
-      bottomValue: '15%',
-    }
-    this.pipeItUp = this.pipeItUp.bind(this)
-    this.update = this.update.bind(this)
-  }
-  
-  pipeItUp() {
-    let newVal = Math.floor(Math.random()*46 + 10);
-    let newValToString = newVal.toString() + '%'
-    this.setState({bottomValue: newValToString});
-    Animated.timing(
-      this.state.pipePosition,
-      {
-        toValue: -75,
-        duration: 1750
-      }).start();
-    setTimeout(this.update, 1750)
-  }
-  update(){
-    this.setState({pipePosition: new Animated.Value(400)})
-  }
-  
-  componentDidMount() {
-    setInterval(this.pipeItUp, 2000)
-  }
-  
+
   render(props) {
     return (
-      <Animated.View style={[styles.container, {left: this.state.pipePosition}]}>
+      <Animated.View style={[styles.container, {left: this.props.PipePosition}]}>
         <View style={styles.pipe}>
-          <View style={[styles.block, {bottom: this.state.bottomValue}]}/>
+          <View style={[styles.block, {bottom: this.props.bottomValue}]}/>
         </View>
       </Animated.View>
     );
   }
 }
-
-//random margin between 50 and 400
 
 const styles = StyleSheet.create({
   container: {
